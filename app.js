2e9efc7ada39e14adbaf3b29df2c49c134c7df36
Jacob <73615055+jacobdev03@users.noreply.github.com>
2023-09-19 09:13:38 +0200

    code refactor

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const body = document.querySelector("body");
-const bookContainer = document.querySelector(".book-container");
-const addBookBtn = document.querySelector(".addBookBtn");
-const form = document.querySelector(".addBookForm");
-const formAdd = document.querySelector("#form-add-btn");
-let myLibrary = [];
+const body = document.querySelector('body');
+const bookContainer = document.querySelector('.book-container');
+const addBookBtn = document.querySelector('.addBookBtn');
+const form = document.querySelector('.addBookForm');
+const formAdd = document.querySelector('#form-add-btn');
+const closeFormBtn = document.querySelector('.close-form');
+const myLibrary = [
+  { title: '1984', author: 'George Orwell', pages: 300, read: true },
+  { title: 'Harry Potter', author: 'J.K Rowling', pages: 317, read: true },
+  { title: '7 habits of highly effective people', author: 'Pioter', pages: 381, read: false },
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -15,57 +20,73 @@ function Book(title, author, pages, read) {
   };
 }
 
-formAdd.addEventListener("click", (e) => {
+const handleBookAdd = (e) => {
   e.preventDefault();
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const read = document.querySelector("#read");
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('#read').checked;
   if (!title || !author || !pages) {
-    alert("Please fill all required fields");
-  } else {
-    form.style.display = "none";
-    form.reset();
-    const book = new Book(title, author, pages, read.checked);
-    myLibrary.push(book);
-    const bookDiv = document.createElement("div");
-    const titleEl = document.createElement("p");
-    titleEl.textContent = title;
-    bookDiv.appendChild(titleEl);
-    const authorEl = document.createElement("p");
-    authorEl.textContent = author;
-    bookDiv.appendChild(authorEl);
-    const pagesEl = document.createElement("p");
-    pagesEl.textContent = pages;
-    bookDiv.appendChild(pagesEl);
-    const statusEl = document.createElement("p");
-    statusEl.classList.add("status");
-    statusEl.textContent = "Read?: " + read.checked;
-    bookDiv.appendChild(statusEl);
-    const removeBtn = document.createElement("button");
-    removeBtn.addEventListener("click", (e) => {
-      removeBook(e.target.parentNode.childNodes[0], myLibrary);
-      console.log(myLibrary);
-    });
-    removeBtn.textContent = "remove";
-    removeBtn.classList.add("remove");
-    bookDiv.appendChild(removeBtn);
-    const readBtn = document.createElement("button");
-    readBtn.textContent = "read";
-    readBtn.classList.add("read");
-    bookDiv.appendChild(readBtn);
-    bookContainer.appendChild(bookDiv);
+    alert('Please fill all required fields');
+    return;
   }
-});
+  form.style.display = 'none';
+  form.reset();
+  const book = new Book(title, author, pages, read);
+  console.log(read.value);
+  myLibrary.push(book);
 
-// To do
+  displayBooks(myLibrary);
+};
 
-function removeBook(title, array) {
+const handleBookRemove = (title, array) => {
   let index = array.findIndex((el) => el.title === title.textContent);
   array.splice(index, 1);
   title.parentNode.remove();
-}
+};
 
-addBookBtn.addEventListener("click", () => (form.style.display = "flex"));
+const displayBooks = (library) => {
+  bookContainer.innerHTML = '';
+  library.forEach((book, index) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.innerHTML = `
 
-console.log(myLibrary);
+        <h3 class='title'>${book.title}</h3>
+        <p>${book.author}</p>
+        <p>Pages: ${book.pages}</p>
+        <p class='status'>Read?: ${book.read}</p>
+      `;
+
+    bookDiv.classList.add('book-card');
+    bookDiv.id = index;
+
+    const removeBtn = document.createElement('button');
+    const readBtn = document.createElement('button');
+
+    removeBtn.addEventListener('click', (e) => {
+      handleBookRemove(e.target.parentNode.childNodes[0], myLibrary);
+      console.log(myLibrary);
+    });
+
+    readBtn.addEventListener('click', (e) => {
+      let readValue = myLibrary[e.target.parentNode.id].read;
+      myLibrary[e.target.parentNode.id].read = readValue === true ? false : true;
+
+      displayBooks(myLibrary);
+    });
+
+    readBtn.textContent = 'read';
+    readBtn.classList.add('btn', 'read');
+    removeBtn.textContent = 'remove';
+    removeBtn.classList.add('btn', 'remove');
+    bookDiv.appendChild(removeBtn);
+    bookDiv.appendChild(readBtn);
+    bookContainer.appendChild(bookDiv);
+  });
+};
+
+displayBooks(myLibrary);
+
+formAdd.addEventListener('click', handleBookAdd);
+addBookBtn.addEventListener('click', () => (form.style.display = 'flex'));
+closeFormBtn.addEventListener('click', () => (form.style.display = 'none'));
